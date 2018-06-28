46463d38f0c9f081407d1824277031e793b7ba85
Edwin Moses <workockmoses@gmail.com>
2018-06-28 18:41:05 +0300

    feat(file manage):check if git folder exists

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
+const files = require("./lib/files");
 
+clear();
+console.info(
+	chalk.yellow(
+		figlet.textSync("Ginit", { horizontal: "full" })
+	)
+);
+
+if (files.directoryExists(".git")) {
+	console.info(chalk.red("Already a git directory!"));
+	process.exit();
+}
